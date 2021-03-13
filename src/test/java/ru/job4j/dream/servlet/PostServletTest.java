10b7f86aed7ce400a6d656b77e076d340f86b39a
dima <test@sd.com>
2021-03-14 01:19:31 +0500

    mockito2

---
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.dream.servlet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.common.internal.PowerMockJUnitRunnerDelegate;
+import org.powermock.*;
+import static org.junit.Assert.*;
+
+@RunWith(PowerMockRunner.class)
+public class PostServletTest {
+
+    @Test
+    public void doGet() {
+    }
+
+    @Test
+    public void doPost() {
+    }
+}
