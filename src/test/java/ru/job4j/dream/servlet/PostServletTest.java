740d0c9353375708659623e3d06657a606ef6b03
dima <test@sd.com>
2021-03-15 21:14:08 +0500

    package org.powermock does not exist

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -1,16 +1,21 @@
 package ru.job4j.dream.servlet;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.modules.junit4.common.internal.PowerMockJUnitRunnerDelegate;
 import org.powermock.*;
+import ru.job4j.dream.store.MemStore;
+
 import static org.junit.Assert.*;
 
 @RunWith(PowerMockRunner.class)
+@PrepareForTest(MemStore.class)
 public class PostServletTest {
 
     @Test
@@ -19,5 +24,8 @@ public class PostServletTest {
 
     @Test
     public void doPost() {
+        Assert.assertEquals(1,1);
+        //хоть и зачеркнуто, но компилируется
+        //assertThat(1,is(1));
     }
 }
