d2ab267c6362d1b58561bd5bc857dd497acc1f51
dima <test@sd.com>
2021-03-17 22:10:50 +0500

    NPE problem

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -18,6 +18,8 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 //
 //import org.powermock.*;
+import ru.job4j.dream.model.Post;
+import ru.job4j.dream.model.User;
 import ru.job4j.dream.store.MemStore;
 import ru.job4j.dream.store.PsqlStore;
 import ru.job4j.dream.store.Store;
@@ -28,13 +30,30 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(PsqlStore.class)
 public class PostServletTest {
 
     @Test
-    public void doGet() {
+    public void doGet() throws ServletException, IOException, SQLException {
+        List<Post> testPosts = new ArrayList<>();
+        User user = new User();
+        //создаем заглушку
+        Store storeStub = new PsqlStoreStub();
+        PowerMockito.mockStatic(PsqlStore.class);
+        //когда инициализируется хранилище - возвращаем заглушку
+        Mockito.when(PsqlStore.instOf()).thenReturn(storeStub);
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        //создаем правило для заглушки (настройка)
+        //подменяем req.getSession().getAttribute("user") на user, иначе выскакивает NPE
+        when(req.getSession().getAttribute("user")).thenReturn("user");
+        //вызываем сервлет для теста
+        new PostServlet().doGet(req,resp);
+        Assert.assertEquals(storeStub.findAllPosts(),testPosts);
     }
 
     @Test
@@ -46,7 +65,8 @@ public class PostServletTest {
         Mockito.when(PsqlStore.instOf()).thenReturn(storeStub);
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
-        //создаем правило для заглушки
+        //создаем правило для заглушки (настройка)
+        //тоесть req.getParameter("id") подменяем на 0
         when(req.getParameter("id")).thenReturn("0");
         when(req.getParameter("name")).thenReturn("JavaJob");
         //вызываем сервлет для теста
